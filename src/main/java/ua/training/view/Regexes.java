6be8275bfa6ac03d04fc7bfb32ca7493d214cea2
Stas <stanislav.holovachuk@gmail.com>
2019-07-28 22:55:42 +0300

    simple cosmetic changes

--- src/main/java/ua/training/view/Regexes.java
+++
@@ -1,5 +0,0 @@
-package ua.training.view;
-
-public interface Regexes {
-    String NICKPAS ="^[A-Za-z0-9]{3,15}$";
-}
--- src/main/java/ua/training/view/TextConst.java
+++
@@ -1,9 +0,0 @@
-package ua.training.view;
-
-public interface TextConst {
-    String WRONGLOGIN = "wrong.login";
-    String WRONGPASS = "wrong.pass";
-    String LOGIN = "login";
-    String ISOCCUPIED = "is.occupied";
-
-}
